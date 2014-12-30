5415fe44d789181774819cd5d51e7444d0595572
Tatsuya Kamijo <kjclev@gmail.com>
2014-12-30 15:14:44 +0900

    Version: 3.0.1.2

--- src/version.h
+++ src/version.h
@@ -12,11 +12,11 @@ extern "C" {
  * CONSTANTS *
  *************/
 
-#define HOEDOWN_VERSION "3.0.1.1"
+#define HOEDOWN_VERSION "3.0.1.2"
 #define HOEDOWN_VERSION_MAJOR 3
 #define HOEDOWN_VERSION_MINOR 0
 #define HOEDOWN_VERSION_REVISION 1
-#define HOEDOWN_VERSION_EXTRAS 1
+#define HOEDOWN_VERSION_EXTRAS 2
 
 
 /*************
