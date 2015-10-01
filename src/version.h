a71661995b35e1089a195ae0f41725a5baee2234
kj <kamijo@ninja.co.jp>
2015-10-02 08:48:30 +0900

    Version: 3.0.5.2

--- src/version.h
+++ src/version.h
@@ -12,11 +12,11 @@ extern "C" {
  * CONSTANTS *
  *************/
 
-#define HOEDOWN_VERSION "3.0.5.1"
+#define HOEDOWN_VERSION "3.0.5.2"
 #define HOEDOWN_VERSION_MAJOR 3
 #define HOEDOWN_VERSION_MINOR 0
 #define HOEDOWN_VERSION_REVISION 5
-#define HOEDOWN_VERSION_EXTRAS 1
+#define HOEDOWN_VERSION_EXTRAS 2
 
 
 /*************
