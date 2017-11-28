6e6333c5c5d736dc1cb443f7290de183cc157506
kj <kjclev@gmail.com>
2017-11-29 07:41:27 +0900

    Updated version to 3.0.7.11

--- src/version.h
+++ src/version.h
@@ -12,11 +12,11 @@ extern "C" {
  * CONSTANTS *
  *************/
 
-#define HOEDOWN_VERSION "3.0.7.10"
+#define HOEDOWN_VERSION "3.0.7.11"
 #define HOEDOWN_VERSION_MAJOR 3
 #define HOEDOWN_VERSION_MINOR 0
 #define HOEDOWN_VERSION_REVISION 7
-#define HOEDOWN_VERSION_EXTRAS 10
+#define HOEDOWN_VERSION_EXTRAS 11
 
 
 /*************
